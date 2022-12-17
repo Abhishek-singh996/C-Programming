4fe833058185170d49a9edd453c400dc388a04a0
Abhishek singh <99178812+Abhishek-singh996@users.noreply.github.com>
2022-12-17 20:04:52 +0530

    "first commit_v0.1"

---
+++ Refernce_variable_type_casting.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+
+using namespace std;
+
+int c = 32;
+int main(){
+    //*************Built in datatypes********
+    // int a, b;
+    // cout<< "Enter the value of a:"<<endl;
+    // cin>>a;
+    // cout<< "Enter the value of b:"<<endl;
+    // cin>>b;
+    // cout<< "Sum of a + b is : "<<(a+b)<<endl;
+    // cout<< "global c is : "<<::c<<endl;
+
+    //*************Literals********
+    // float d= 34.4f;
+    // long double e = 34.4L;
+    // cout<<"size of 34.4 is : "<<sizeof(34.4)<<endl;
+    // cout<<"size of 34.4f is : "<<sizeof(34.4f)<<endl;
+    // cout<<"size of 34.4fF is : "<<sizeof(34.4F)<<endl;
+    // cout<<"size of 34.4l is : "<<sizeof(34.4l)<<endl;
+    // cout<<"size of 34.4L is : "<<sizeof(34.4L)<<endl;
+
+    //*********************
+
+    return 0;
+
+}
---
+++ operators.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a = 1,b = 2;
+    cout<< "True value of a and b is: "<<a <<'\n' <<b<<endl;
+    cout<< "Value of sum is : "<<(a+b)<<endl;
+    cout<< "Value of subtraction is : "<<(a-b)<<endl;
+    cout<< "Value of division operator is : "<<(a/b)<<endl;
+    cout<< "Value of multiplication is : "<<(a*b)<<endl;
+    cout<< "check less than : "<<(a<b)<<endl;
+    cout<< "check greater than : "<<(a>b)<<endl; 
+    cout<< "check greater than : "<<(a>b)<<endl;
+    cout<< "check less than or equal to : "<<(a<=b)<<endl;
+    cout<< "check greater than or equal to : "<<(a>=b)<<endl;
+    cout<< "value of a++ : "<<(a++)<<endl;
+    cout<< "value of a-- : "<<(a--)<<endl;
+    cout<< "value of --a : "<<(--a)<<endl;
+    cout<< "value of ++a : "<<(++a)<<endl;
+    cout<< "value of b++ : "<<(b++)<<endl;
+    cout<< "value of b-- : "<<(b--)<<endl;
+    cout<< "value of ++b : "<<(++b)<<endl;
+    cout<< "value of --b : "<<(--b)<<endl;
+    // logical operators
+    cout<<"The value of logical operator is : "<<((a==b) && (a!=b))<<endl;
+
+
+}
+
